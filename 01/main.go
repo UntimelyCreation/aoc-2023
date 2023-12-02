0fee6105e8c69f5858c244cc6d6a8a405f1efd7c
Paul Graydon <untimely.creation97@proton.me>
2023-12-01 19:48:07 -0800

    Initial commit

---
+++ 01/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+func calculate_calibration_values(path string, pattern string) int {
+	regex := regexp.MustCompile(pattern)
+
+	digits := map[string]int{
+		"one":   1,
+		"1":     1,
+		"two":   2,
+		"2":     2,
+		"three": 3,
+		"3":     3,
+		"four":  4,
+		"4":     4,
+		"five":  5,
+		"5":     5,
+		"six":   6,
+		"6":     6,
+		"seven": 7,
+		"7":     7,
+		"eight": 8,
+		"8":     8,
+		"nine":  9,
+		"9":     9,
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	result := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := []int{}
+
+		for i := range line {
+			sub := line[i:]
+			if match := regex.FindStringIndex(sub); match != nil {
+				l, r := match[0], match[1]
+				matches = append(matches, digits[sub[l:r]])
+			}
+		}
+
+		result += 10*matches[0] + matches[len(matches)-1]
+	}
+
+	return result
+}
+
+func main() {
+	calibration_sum_1 := calculate_calibration_values("01/input.txt", `^\d`)
+	fmt.Print("Part 1 solution: ", calibration_sum_1, "\n")
+
+	calibration_sum_2 := calculate_calibration_values("01/input.txt", `^one|^two|^three|^four|^five|^six|^seven|^eight|^nine|^\d`)
+	fmt.Print("Part 2 solution: ", calibration_sum_2, "\n")
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Advent of Code 2023
+
+Solutions for the Advent of Code 2023 problems, written in Go.
+
+This is my first project using Go: I intend to learn and practice the language instead of optimizing the solutions.
+
+
+## Usage
+
+### Prerequisites
+
+This project makes use of **Go 1.21**.
+
+
+### Build and run solutions
+
+The repository is structured into subdirectories, each one named after the corresponding day in the calendar and containing a main file `main.go`.
+
+In a given subdirectory, place the corresponding input in a `.txt` file, and adjust its path in the `main.go` file accordingly.
+
+Then, simply run the `main.go` file.
+
+For example, to produce the solutions for Day 1:
+
+```bash
+go run 01/main.go
+```
+
+
+## License
+
+Distributed under the MIT License.
